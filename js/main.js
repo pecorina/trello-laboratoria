b699c4a9c5022fb443a5e41d8db612b743db6a3e
pecorina <constanza.pecori@gmail.com>
2017-05-18 01:33:24 -0400

    envio lo q hice aunq no me haya salido bien

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
+function agregaTrello(){
 
 
+   
+   var textoInput = document.getElementById("textoInput");
+   var btnNuevo = document.getElementById("btn-agregar");
+
+var btnAgregar = function(){
+	alert("agrego");
+};
+var comprobarInput = function(){
+	alert("compruebo");
+};
+
+
+btnNuevo.addEventListener("click", btnAgregar);
+textoInput.addEventListener("click", comprobarInput);
+
+}
+
+
+/*
 var container = document.getElementById("boxis");
 var textrello= document.createElement("textrello");
+//var box = document.getElementById("box");
 textrello.setAttribute("placeholder", "Añadir Tarea");
 textrello.classList.add("text");
 //asignamos padres, primero es el padre y despues es el hijo siempre.
@@ -13,7 +34,6 @@ var contienelista=document.getElementById("box");
 var listado= document.createElement("p");
 var lista=document.createTextNode("text");//variebles van sin comillas.
 listado.appendChild(lista);
-contienelista.appendChild.(listado);
+contienelista.appendChild(listado);
 
-
-}
+*/
