7d6d531430d9194bace6badc177c84d5918dffac
pecorina <constanza.pecori@gmail.com>
2017-05-17 22:28:19 -0400

    intento hacer la caje de texto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-alert("halojaaa");
 
-document.getElementById("telo").addEventListener("click", function(){
-	alert("Hola Mundo")
-});
+
+var container = document.getElementById("boxis");
+var textrello= document.createElement("textrello");
+textrello.setAttribute("placeholder", "Añadir Tarea");
+textrello.classList.add("text");
+//asignamos padres, primero es el padre y despues es el hijo siempre.
+container.appendChild(textrello);
+
+function ingresar(){
+var text= textrello.value;
+var contienelista=document.getElementById("box");
+var listado= document.createElement("p");
+var lista=document.createTextNode("text");//variebles van sin comillas.
+listado.appendChild(lista);
+contienelista.appendChild.(listado);
+
+
+}
